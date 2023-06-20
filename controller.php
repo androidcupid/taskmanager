95b5aa5746c44954212f6e951a9481b1cb4da466
Sylvester Antwi-Boasiako <sylvesterantwiboasiako@gmail.com>
2023-06-20 21:25:56 +0000

    login validation for index page

--- controller.php
+++ controller.php
@@ -1,5 +1,25 @@
 <?php
 include('includes/connection.php');
 
-if(isset($_POST['submit']))
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $us_check = "SELECT * FROM admin WHERE username ='$username'";
+    $rs_check= mysqli_query($connection,$us_check);
+
+    if($rs_check){
+        if(mysqli_num_rows($rs_check)>0){
+            while($rows=mysqli_fetch_array($rs_check)){
+                if($password==MD5($password)){
+                    header('location: dashboard.php');
+                }else{
+                    echo '<div class="alert alert-danger>
+                            <p>Invalid Password! Try Again!</p>
+                        </div>"';
+                }
+            }
+        }
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php 
-include('/includes/header.php');
-include('/includes/connection.php');
+include('includes/header.php');
+include('includes/connection.php');
 ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <form class="form-group" action="controller.php">
-                    <input type="text" class="form-control" name="username" required/>
-                    <input type="text" class="form-control" name="password" required/>
+            
+            <div class="col-md-4 signin">
+                <h5 class="text-center">Sign In</h5>
+                <form class="form-group" action="controller.php" role="form">
+                    <input type="text" class="form-control" name="username" placeholder="Username" required/>
+                    <input type="password" class="form-control" name="password" placeholder="Password" required/>
 
-                    <button type="submit" class="btn btn-sm" name="login">Log In</button>
+                    <button class="btn btn-sm" name="login">Log In</button>
                 </form>
             </div>
         </div>
     </div>
     <?php 
-include('/includes/footer.php');
+include('includes/footer.php');
 ?>
