0689ec284a6f402dc378494a0d23d70243c80a52
Sylvester Antwi-Boasiako <sylvesterantwiboasiako@gmail.com>
2023-06-20 21:48:29 +0000

    added dashboard page

--- controller.php
+++ controller.php
@@ -11,7 +11,7 @@ if(isset($_POST['login'])){
     if($rs_check){
         if(mysqli_num_rows($rs_check)>0){
             while($rows=mysqli_fetch_array($rs_check)){
-                if($password==MD5($password)){
+                if(MD5($password)==$rows['password']){
                     header('location: dashboard.php');
                 }else{
                     echo '<div class="alert alert-danger>
